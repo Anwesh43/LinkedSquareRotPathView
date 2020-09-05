846e5a67ee1f73d8c6c7b6a51fed0bad3ea4ced7
Anwesh43 <anweshthecool0@gmail.com>
2020-09-06 03:10:06 +0530

    adding view in activity and fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedsquarerotpathview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarerotpathview/MainActivity.kt
@@ -2,11 +2,19 @@ package com.anwesh.uiprojects.linkedsquarerotpathview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import com.anwesh.uiprojects.squarerotpathview.SquareRotPathView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        SquareRotPathView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
