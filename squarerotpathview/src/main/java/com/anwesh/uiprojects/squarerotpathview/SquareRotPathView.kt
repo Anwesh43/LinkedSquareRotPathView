b1ff3ef8faf396a75bea2079183b60712d368d10
Anwesh43 <anweshthecool0@gmail.com>
2020-09-06 02:30:33 +0530

    adding Canvas extension methods

--- squarerotpathview/src/main/java/com/anwesh/uiprojects/squarerotpathview/SquareRotPathView.kt
+++ squarerotpathview/src/main/java/com/anwesh/uiprojects/squarerotpathview/SquareRotPathView.kt
@@ -26,8 +26,51 @@ val strokeFactor : Int = 90
 val sizeFactor : Float = 2.9f
 val delay : Long = 20
 val backColor : Int = Color.parseColor("#BDBDBD")
+val rFactor : Float = 5.6f
+val deg : Float = 90f
 
 fun Int.inverse() : Float = 1f / this
 fun Float.maxScale(i : Int, n : Int) : Float = Math.max(0f, this - i * n.inverse())
 fun Float.divideScale(i : Int, n : Int) : Float = Math.min(n.inverse(), maxScale(i, n)) * n
 fun Float.sinify() : Float = Math.sin(this * Math.PI).toFloat()
+
+fun Canvas.draweSquareRotPath(scale : Float, w : Float, h : Float, paint : Paint) {
+    val sf : Float = scale.sinify()
+    val sf1 : Float = sf.divideScale(0, parts)
+    val sf2 : Float = sf.divideScale(1, parts)
+    val sf3 : Float = sf.divideScale(2, parts)
+    val sf4 : Float = sf.divideScale(3, parts)
+    val size : Float = Math.min(w, h) / sizeFactor
+    val r : Float = Math.min(w, h) / rFactor
+    val kSize : Float = w / 2 - size
+    save()
+    translate(w / 2, h / 2)
+    for (j in 0..1) {
+        save()
+        scale(1f - 2 * j, 1f)
+        save()
+        translate(-kSize, 0f)
+        save()
+        rotate(90f * sf2)
+        drawLine(0f, 0f, 0f, -kSize * sf1, paint)
+        restore()
+        drawLine(0f, 0f, -size * sf1, 0f, paint)
+        restore()
+        save()
+        translate(-w / 2 + (w / 2 - r) * sf4, 0f)
+        drawRect(RectF(0f, -r * sf3, r, 0f), paint)
+        restore()
+        restore()
+    }
+    restore()
+}
+
+
+fun Canvas.drawSRPNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    paint.color = colors[i]
+    paint.strokeCap = Paint.Cap.ROUND
+    paint.strokeWidth = Math.min(w, h) / strokeFactor
+    draweSquareRotPath(scale, w, h, paint)
+}
