3177c16f311a3c9cd2002326919d607c5bd31ceb
Anwesh43 <anweshthecool0@gmail.com>
2020-09-06 01:46:52 +0530

    adding project setup

---
+++ app/src/main/java/com/anwesh/uiprojects/linkedsquarerotpathview/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.anwesh.uiprojects.linkedsquarerotpathview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ squarerotpathview/src/main/java/com/anwesh/uiprojects/squarerotpathview/SquareRotPathView.kt
@@ -0,0 +1,6 @@
+package com.anwesh.uiprojects.squarerotpathview
+
+/**
+ * Created by anweshmishra on 06/09/20.
+ */
+
